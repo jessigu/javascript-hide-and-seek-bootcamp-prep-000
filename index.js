c5631ba998eb7bb7bf99e2d140c90462b38358ee
Jess Guttenberg <jessigu@gmail.com>
2018-08-28 20:07:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector (selector) {
+  return document.querySelector('div#nested');
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+//parseInt?
+function increaseRankBy(n) {
+  const li = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < li.length; i++) {
+    li[i].innerHTML = i + n;
+  }
+  return li;
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelectorAll('div')[3];
+}
