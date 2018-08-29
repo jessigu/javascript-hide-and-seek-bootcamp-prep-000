1970a2f9e66676e019f342d248c1f798bea69ac8
Jess Guttenberg <jessigu@gmail.com>
2018-08-29 17:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ function deepestChild() {
 
 
 /*
+<body>
+    <main id="app">
+      <ul class="unranked-list">
+        <li>2</li>
+        <li>5</li>
+        <li>4</li>
+      </ul>
+
+      <ul class="ranked-list">
+        <li>1</li>
+        <li>2</li>
+      </ul>
+
+      <ul class="ranked-list">
+        <li>12</li>
+        <li>11</li>
+        <li>10</li>
+      </ul>
+
 const lis = document
   .getElementById("app")
   .querySelectorAll("ul.ranked-list li");
