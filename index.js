5ac880d4ad0cbe4528296b317397c4067bc3ab45
Jess Guttenberg <jessigu@gmail.com>
2018-08-29 18:30:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-function getFirstSelector (selector) {
-  return document.getElementById('nested').querySelector('div');
-}
+const getFirstSelector = selector => document.querySelector(selector);
 
 function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target');
-}
-
-//parseInt?
-function increaseRankBy(n) {
-  const li = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-  for (let i = 0; i < li.length; i++) {
-    li[i].innerHTML = parseInt(i, n);
-  }
 }
 
 function deepestChild() {
   return document.getElementById('grand-node').querySelectorAll('div')[3];
 }
 
-
-/*
-const lis = document
-  .getElementById("app")
-  .querySelectorAll("ul.ranked-list li");
- 
-for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + 1).toString();
-}*/
+function increaseRankBy(n) {
+  const li = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < li.length; i++) {
+    li[i].innerHTML = parseInt(li[i].innerHTML) + n;
+  }
+}
